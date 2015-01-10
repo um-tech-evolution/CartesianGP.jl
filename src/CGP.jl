cab873816687088de333e9441c594843abf3677b
George Lesica <george@lesica.com>
2015-01-10 19:23:32 -0300

    Implement additional evaluation functions and outputs.

--- src/CGP.jl
+++ src/CGP.jl
@@ -41,18 +41,24 @@ type InputNode <: Node
     index::Integer
 end
 
-function blank_input(index::Integer)
-    return InputNode(index)
-end
-
 type InteriorNode <: Node
     func::Func
     inputs::Vector{Node}
     active::Bool
-    output::Uint64
 end
 
-function random_node(p::Parameters, funcs::Vector{Func}, inputs::Vector{InputNode}, nodes::Vector{InteriorNode}, index::Integer)
+type OutputNode <: Node
+    input::Union(InputNode, InteriorNode)
+end
+
+type Chromosome
+    inputs::Vector{InputNode}
+    nodes::Vector{InteriorNode}
+    outputs::Vector{OutputNode}
+    fitness::FloatingPoint
+end
+
+function random_node(p::Parameters, funcs::Vector{Func}, chrom::Chromosome, index::Integer)
     perlevel = (p.numnodes / p.numlevels)
     func = funcs[rand(1:end)]
     level = div(index - 1, perlevel) + 1
@@ -65,44 +71,58 @@ function random_node(p::Parameters, funcs::Vector{Func}, inputs::Vector{InputNod
         src = rand(start:stop)
         if src < 1
             # Chose an input
-            nodeinputs[i] = inputs[-src + 1]
+            nodeinputs[i] = chrom.inputs[-src + 1]
         else
-            nodeinputs[i] = nodes[src]
+            nodeinputs[i] = chrom.nodes[src]
         end
     end
 
     # Defaults
     active = true
-    output = 0
 
-    return InteriorNode(func, nodeinputs, active, output)
+    return InteriorNode(func, nodeinputs, active)
 end
 
-type Chromosome
-    inputs::Vector{InputNode}
-    nodes::Vector{InteriorNode}
-    fitness::FloatingPoint
+function random_output(p::Parameters, nodes::Vector{InteriorNode})
 end
 
 function blank_chromosome(p::Parameters)
     inputs = Array(InputNode, p.numinputs)
     nodes = Array(InteriorNode, p.numnodes)
+    outputs = Array(OutputNode, p.numoutputs)
     fitness = 0.0
-    return Chromosome(inputs, nodes, fitness)
+    return Chromosome(inputs, nodes, outputs, fitness)
 end
 
 function random_chromosome(p::Parameters, funcs::Vector{Func})
     chromosome = blank_chromosome(p)
 
     for i = 1:length(chromosome.inputs)
-        chromosome.inputs[i] = blank_input(i)
+        chromosome.inputs[i] = InputNode(i)
     end
 
     for i = 1:length(chromosome.nodes)
-        chromosome.nodes[i] = random_node(p, funcs, chromosome.inputs, chromosome.nodes, i)
+        chromosome.nodes[i] = random_node(p, funcs, chromosome, i)
     end
 
     return chromosome
+end
+
+function evaluate_node(node::InputNode, context::Vector{Integer})
+    return context[node.index]
+end
+
+function evaluate_node(node::InteriorNode, context::Vector{Integer})
+    func = node.func
+    return apply(func.func, [evaluate_node(n) for n = node.inputs[1:func.maxinputs]])
+end
+
+function evaluate_node(node::OutputNode, context::Vector{Integer})
+    return evaluate_node(node.input, context)
+end
+
+function execute_chromosome(chromosome::Chromosome, context::Vector{Integer})
+    return [evaluate_node(n, context) for n = chromosome.outputs]
 end
 
 end
