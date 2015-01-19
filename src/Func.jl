25007208817ae1504851139bf6a747c00d6635d8
George Lesica <george@lesica.com>
2015-01-19 09:41:09 -0300

    Add new functions from AHW.

--- src/Func.jl
+++ src/Func.jl
@@ -5,7 +5,12 @@ type Func
     arity::Integer
 end
 
+const AND = Func(&, 2)
+const OR = Func(|, 2)
+const XOR = Func($, 2)
+const TRUE = Func(() -> 0xf, 0)
+
 function default_funcs()
-    return [Func(&, 2), Func(|, 2)]
+    return [AND, OR, XOR, TRUE]
 end
 
