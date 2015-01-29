c38e9c7b1f2178de89a588a7e955704303b142ee
Alden Wright <github@fastmail.us>
2015-01-29 16:25:16 -0700

    Added the function print_chromosome which prints the chromosome in a    condensed text format on one line.  There is an option to print only active nodes.

--- src/Chromosome.jl
+++ src/Chromosome.jl
@@ -1,6 +1,6 @@
 import Base.getindex
 
-export Chromosome, getindex
+export Chromosome, getindex, print_chromosome
 
 type Chromosome
     params::Parameters
@@ -28,4 +28,42 @@ function getindex(c::Chromosome, level::Integer, index::Integer)
     end
 
     return c.interiors[level, index]
+end
+
+# Prints the chromosome in a compact text format (on one line).
+# Active notes are indicated with a "+" and inactive notes with a "*".
+# If active_only is true, then only the active notes are shown.
+function print_chromosome(c::Chromosome, active_only::Bool=false)
+    for i = 1:length(c.inputs)
+        active = c.inputs[i].active ? "+" : "*"
+        if c.inputs[i].active || !active_only
+            print("[in",i,active,"] ")
+        end
+    end
+
+    for i = 1:c.params.numlevels
+        for j = 1:c.params.numperlevel
+           active = c.interiors[i,j].active ? "+" : "*"
+           if c.interiors[i,j].active || !active_only
+               print("[")
+               for k = 1:length(c.interiors[i,j].inputs)
+                   if c.interiors[i,j].inputs[k][1] == 0
+                       print("(in", c.interiors[i,j].inputs[k][2],")")
+                   else
+                       print(c.interiors[i,j].inputs[k])
+                   end
+               end
+               print("\"",c.interiors[i,j].func.name,"\"",(i,j),active,"] ")
+           end
+        end
+    end
+
+    for i = 1:length(c.outputs)
+        active = c.outputs[i].active ? "+" : "*"
+        if c.outputs[i].active || !active_only
+            print("[",c.outputs[i].input,"out",i,active,"] ")
+        end
+    end
+    println()
+    return
 end
