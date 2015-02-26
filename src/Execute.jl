f28d38009a41280b9c96d8b2e05cecffaee5da83
George Lesica <george@lesica.com>
2015-02-26 13:17:28 -0300

    Clean up code and improve readability.

--- src/Execute.jl
+++ src/Execute.jl
@@ -26,27 +26,30 @@ function execute_chromosome(c::Chromosome, context::Vector{BitString})
 end
 
 # Supplies the standard context for up to 4 inputs
-function execute_chromosome(chromosome::Chromosome)
-   mask = output_mask(chromosome.params.numinputs)
-   if chromosome.params.numinputs == 2
-       result = execute_chromosome(chromosome, [convert(BitString,0xC), convert(BitString,0xA)])
-   elseif chromosome.params.numinputs == 3
-       result = execute_chromosome(chromosome, [convert(BitString,0xF0),convert(BitString,0xCC), convert(BitString,0xAA)])
-   elseif chromosome.params.numinputs == 4
-       result = execute_chromosome(chromosome, [convert(BitString,0xFF00),convert(BitString,0xF0F0),
-           convert(BitString,0xCCCC), convert(BitString,0xAAAA)])
-   else
-       println("Too many inputs in execute_chromosome")
-   end
-   for i in 1:chromosome.params.numoutputs
-      result[i] = result[i] & mask
-   end
-   return result
+function execute_c(c::Chromosome)
+    params = c.params
+    mask = output_mask(params.numinputs)
+
+    if params.numinputs == 1
+        ctx = [0b10]
+    elseif params.numinputs == 2
+        ctx = [0b1100, 0b1010]
+    elseif params.numinputs == 3
+        ctx = [0b11110000, 0b11001100, 0b10101010]
+    elseif params.numinputs == 4
+        ctx = [0b1111111100000000, 0b1111000011110000, 0b1100110011001100, 0b1010101010101010]
+    else
+        error("Too many inputs, max is 4")
+    end
+
+    result = execute_chromosome(c, [convert(BitString, x) for x = ctx])
+
+    return [x & mask for x = result]
 end
 
 # bitstring mask for one output of the packed representation
 function output_mask(num_inputs)
-   one = convert(BitString,0x1)
+   one = convert(BitString, 0x1)
    mask = one
    for i in 1:(2^num_inputs-1)
       mask <<= 1
