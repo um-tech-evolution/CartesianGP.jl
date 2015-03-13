af54053086ef522bf0ceae057bba9fff3c37c326
George Lesica <george@lesica.com>
2015-03-13 18:35:19 -0300

    Fix runtests.sh to run the working code and work with Vagrant.

--- runtests.sh
+++ runtests.sh
@@ -1,3 +1,9 @@
-#! /usr/bin/env sh
+#! /usr/bin/env bash
 
-julia -e 'Pkg.clone(pwd()); Pkg.test("CGP")'
+# Get the directory where the script is located. Should be the
+# repository root directoy.
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Run the tests based on the code in the working directory, not just
+# what has been committed.
+cd "$DIR/test" && julia -e 'include("../src/CGP.jl"); include("runtests.jl");'
