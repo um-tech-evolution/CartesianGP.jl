8b2238a4f64092c5e501f8d1d15105bc5f9b4105
George Lesica <george@lesica.com>
2015-03-22 19:20:11 -0300

    Remove pseudo code.

--- src/Evolution.jl
+++ src/Evolution.jl
@@ -1,34 +1,3 @@
-# mu+lambda Evolution-strategy algorthm.
-
-# Given:  
-# mu:     base population size preserved between generations.  Usually 1 in standard CGP
-# lambda:  number of added individuals undergoing fitness evaluation in each generation
-# goal:    truth table representation of the Boolean function that the circuit is supposed to compute
-# perfect: the perfect fitness of a circuit, i. e., the fitness of a circuit that exactly computes the goal
-
-#   for i in 1:mu+lambda parallel do
-#     pop[i]= new Chromosome
-#     truth_table = execute_chromosome(pop[i])
-#     fit[i] = fitness(truth_table,goal)
-#   end
-#   for t in 1:num_generations
-#     rank = permutation of 1:mu+lambda so that fit[rank[i]] >= fit[rank[j]] for i in 1:mu and all j > i
-#     if fit[rank[1]] == perfect return(pop[rank[1]]) end
-#     for i in 1:mu
-#       shallow copy pop[rank[i]] to newpop[i]  # copy mu most fit chromosomes to first mu positions of newpop
-#       newfit[i] = fit[rank[i]]
-#     end
-#     for j in mu:mu+lambda parallel do
-#       deep copy newpop[j % mu + 1] to newpop[j]   # duplicate mu most fit chromosomes
-#       mutate newpop[j]
-#       truth_table = execute_chromosome(newpop[j])
-#       newfit[j] = fitness(truth_table,goal)
-#     end
-#     pop = newpop
-#     fit = newfit
-#   end
-#   return chromosome[rank[1]]
-
 export mu_lambda
 
 function mu_lambda(p::Parameters, goal::Goal, gens::Integer, perfect::Float64)
