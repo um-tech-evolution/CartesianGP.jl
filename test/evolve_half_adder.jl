391e4db5f866907567b4f142597d939edb963b31
George Lesica <george@lesica.com>
2015-03-23 01:15:19 -0300

    Add full adder test.

--- test/evolve_half_adder.jl
+++ test/evolve_half_adder.jl
@@ -3,7 +3,9 @@ include("../src/CGP.jl")
 using CGP
 using Base.Test
 
-function parameters()
+# TODO: Add correctness tests
+
+function parameters_half_adder()
     numperlevel = 2
     numlevels = 1
     numlevelsback = 1
@@ -20,20 +22,43 @@ function parameters()
 end
 
 function goal_half_adder()
-    g = Goal(2, (0b0110, 0b1000))
-
-    return g
+    return Goal(2, (0b0110, 0b1000))
 end
 
-p = parameters()
+function parameters_full_adder()
+    numperlevel = 3
+    numlevels = 3
+    numlevelsback = 2
+    numinputs = 3
+    numoutputs = 2
+
+    mu = 1000
+    lambda = 1000
+    mutrate = 0.05
+    targetfitness = 1.0
+    funcs = default_funcs()
+
+    return Parameters(mu, lambda, mutrate, targetfitness, numinputs, numoutputs, numperlevel, numlevels, numlevelsback, funcs)
+end
+
+function goal_full_adder()
+    return Goal(3, (0b10010110, 0b11101000))
+end
+
+p = parameters_half_adder()
 g = goal_half_adder()
 n = 100
-
-# TODO: Add correctness tests
 r = mu_lambda(p, g, n)
 
 @assert fitness(r[1], g) == p.targetfitness
 @assert r[2] <= n
-
 print_chromosome(r[1])
 
+p = parameters_full_adder()
+g = goal_full_adder()
+n = 100000
+r = mu_lambda(p, g, n)
+
+@assert fitness(r[1], g) == p.targetfitness
+@assert r[2] <= n
+print_chromosome(r[1])
