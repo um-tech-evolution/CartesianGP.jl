66074c1eafccbc2715a4e617b39145f30ef0e702
Alden Wright <github@fastmail.us>
2015-03-28 19:51:14 -0600

    Revised evolve_adders.jl  to use the mutation version in Mutate.jl.  Now evolves the half adder and the one-output full adder.

--- test/evolve_adders.jl
+++ test/evolve_adders.jl
@@ -1,4 +1,5 @@
 include("../src/CGP.jl")
+include("../src/Mutate.jl")
 
 using CGP
 using Base.Test
@@ -30,14 +31,51 @@ end
 
 p = parameters_half_adder()
 g = goal_half_adder()
-n = 100
+n = 1000
 r = mu_lambda(p, g, n)
 
-println(fitness(r[1], g))
+println("running mu_lambda on half adder")
+println("fitness:",fitness(r[1], g),"  gens:",r[2])
 @assert fitness(r[1], g) == p.targetfitness
 @assert r[2] <= n
 print_chromosome(r[1])
 
+#Half (single-output) full adder
+function parameters_half_full_adder()
+    numperlevel = 1
+    numlevels = 10
+    numlevelsback = numlevels
+    numinputs = 3
+    numoutputs = 1
+
+    mu = 1
+    lambda = 4
+    mutrate = 0.12
+    targetfitness = 1.0
+    funcs = [AND, OR, XOR]
+    fitfunc = hamming_max
+
+    return Parameters(mu, lambda, mutrate, targetfitness, numinputs, numoutputs, numperlevel, numlevels, numlevelsback, funcs, fitfunc)
+end
+
+function goal_half_full_adder()
+    return Goal(3, (0xe8, ))
+end
+
+p = parameters_half_full_adder()
+g = goal_half_full_adder()
+n = 100000
+println("runing mu_lambda on half full adder")
+r = mu_lambda(p, g, n)
+#println("trying evolve on half full adder")
+#r = evolve(p, g, n)
+
+println("fitness:",fitness(r[1], g),"  gens:",r[2])
+@assert fitness(r[1], g) == p.targetfitness
+@assert r[2] <= n
+print_chromosome(r[1])
+
+#=
 # Full adder
 
 function parameters_full_adder()
@@ -69,3 +107,4 @@ r = mu_lambda(p, g, n)
 @assert fitness(r[1], g) == p.targetfitness
 @assert r[2] <= n
 print_chromosome(r[1])
+=#
