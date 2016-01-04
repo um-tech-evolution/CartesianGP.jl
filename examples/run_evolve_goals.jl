e99b6fd9ba7298475000d10aa525b4cae5e1075f
Alden Wright <github@fastmail.us>
2016-01-04 06:21:53 -0700

    Did updates to fix deprecated type warning message. For example:  "WARNING: Base.String is deprecated, use AbstractString instead." Rewrote  examples/EvolveGoals.jl  to evolve a chosen number of randomly chosen single-output goals     in addition to evolving all single-output 2-input and 3-input goals.     A random list of single-output 4-input goals can be evolved. Modified   examples/EvolveGoals.jl  to show how many runs of a goal successfully found the goal. Revised parallel programming aspects of examples/EvolveGoals.jl to reduce the use of parallel programming     functions used and directives to a minimum.  Reduced the use of global variables to 2 variables. Improved in-line documentation of examples/EvolveGoals.jl. Added  examples/run_evolve_goals.jl  which gives an easy way to run   examples/EvolveGoals.jl.

---
+++ examples/run_evolve_goals.jl
@@ -0,0 +1,30 @@
+#Example command line to run this program on multiple processors:
+# julia -p 4 "run_evolve_goals.jl"
+
+@everywhere include("../src/CGP.jl")
+@everywhere using CGP
+@everywhere include("EvolveGoals0.jl")
+
+# Example runs.  Uncomment one of these lines
+#run_evolve_goals("prun",2,3,10,4)
+#run_evolve_goals("rrun",4,15,200,3,4)
+#run_evolve_goals("erun",3,14,50,6)
+run_evolve_goals("erun",3,14,50,6,4)
+#run_evolve_goals("erun",3,14,50,20)
+
+# In run_evolve_goals("frun",3,14,50,6),
+#   numinputs = 3
+#   random seed = 14
+#   genome size = 50
+#   number of repetitions per goal = 6
+# Evolves all goals of length 3 with 6 repetitions per goal
+
+# In run_evolve_goals("frun",4,14,200,3,4),
+#   numinputs = 4
+#   random seed = 14
+#   genome size = 200
+#   number of repetitions per goal = 3
+#   number of random goals = 4
+
+# An alternative that does not involve running this file
+# julia -p 2  -L EvolveGoals.jl -e "run_evolve_goals(\"prun\",3,16,20,3,4)"
