5edcebd9b8d11bf9b9c44821bfbcc2058924c313
George Lesica <george@lesica.com>
2016-06-19 14:11:47 -0600

    Remove vagrant file and update test script.

--- runtests.sh
+++ runtests.sh
@@ -1,32 +1,4 @@
-#! /usr/bin/env bash
+#!/bin/sh
 
-# Get the directory where the script is located. Should be the
-# repository root directoy.
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+julia test/runtests.jl
 
-usage() {
-    echo "./runtests.sh [{nightlies,releases}]"
-}
-
-runtests() {
-    echo "Environment: $1"
-    vagrant ssh "$1" -c '/vagrant/runtests.sh'
-}
-
-if [ "$#" -eq 1 ]; then
-    if [ "$1" == "nightlies" ] || [ "$1" == "releases" ]; then
-	runtests $1
-	exit 0
-    elif [ "$1" == "all" ]; then
-	runtests "nightlies"
-	runtests "releases"
-	exit 0
-    else
-	usage
-	exit 1
-    fi
-fi
-
-# Run the tests based on the code in the working directory, not just
-# what has been committed.
-cd "$DIR/test" && julia -e 'include("../src/CartesianGP.jl"); include("runtests.jl");'
